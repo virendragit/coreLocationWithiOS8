8075d5a15caf79daa36cfa09bc67c0dd4da9a9bb
Virendra <virendra@pc268625.local>
2015-07-21 18:05:56 +0530

    second commit

--- coreLocationMapView/AppDelegate.m
+++ coreLocationMapView/AppDelegate.m
@@ -17,6 +17,8 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    
+    //added comment
     return YES;
 }
 
