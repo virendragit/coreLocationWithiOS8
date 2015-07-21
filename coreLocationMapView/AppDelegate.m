cc408ad7f4578cc3defb12cb9e679949f11825d6
Virendra <virendra@pc268625.local>
2015-07-21 18:08:03 +0530

    removed comment

--- coreLocationMapView/AppDelegate.m
+++ coreLocationMapView/AppDelegate.m
@@ -18,7 +18,7 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
     
-    //added comment
+    
     return YES;
 }
 
